29e5d60e84d68880f2f2b299fa0e6595da58e5d9
francoMG <francomolocheg99@gmail.com>
2021-11-11 17:25:57 -0500

    [/] Fish-farm: Información de wireframes y estilos

--- src/app/components/admin/fish-farm-details/fish-farm-details.component.ts
+++ src/app/components/admin/fish-farm-details/fish-farm-details.component.ts
@@ -1,5 +1,14 @@
 import { Component, OnInit } from '@angular/core';
 
+type Devices = {
+  title: string;
+  sensors: {
+    isActive: boolean;
+    imageSrc: string;
+    title: string;
+  }[];
+}[];
+
 @Component({
   selector: 'app-fish-farm-details',
   templateUrl: './fish-farm-details.component.html',
@@ -7,6 +16,59 @@ import { Component, OnInit } from '@angular/core';
 })
 export class FishFarmDetailsComponent implements OnInit {
   constructor() {}
+
+  deviceData: Devices = [
+    {
+      title: 'Sensores de temperatura',
+      sensors: [
+        {
+          isActive: true,
+          imageSrc: 'assets/image-frame.png',
+          title: 'Device X',
+        },
+        {
+          isActive: true,
+          imageSrc: 'assets/image-frame.png',
+          title: 'Device Y',
+        },
+      ],
+    },
+    {
+      title: 'Sensores de pH',
+      sensors: [
+        {
+          isActive: true,
+          imageSrc: 'assets/image-frame.png',
+          title: 'Device X',
+        },
+      ],
+    },
+    {
+      title: 'Sensores de oxígeno disuelto',
+      sensors: [
+        {
+          isActive: false,
+          imageSrc: 'assets/image-frame.png',
+          title: 'Device X',
+        },
+        {
+          isActive: true,
+          imageSrc: 'assets/image-frame.png',
+          title: 'Device Y',
+        },
+      ],
+    },
+    {
+      title: 'Compuertas',
+      sensors: [
+        {
+          isActive: true,
+          imageSrc: 'assets/image-frame.png',
+          title: 'Device X',
+        },
+      ],
+    },
+  ];
 
   ngOnInit(): void {}
 }
