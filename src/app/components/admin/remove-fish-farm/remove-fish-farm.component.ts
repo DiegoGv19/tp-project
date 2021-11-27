efbf614e4a25602efdcda2732e023a4b30fe2de4
francoMG <francomolocheg99@gmail.com>
2021-11-27 14:48:16 -0500

    [/] Remove-fish: creating component

---
+++ src/app/components/admin/remove-fish-farm/remove-fish-farm.component.ts
@@ -0,0 +1,66 @@
+import { Component, Inject } from '@angular/core';
+import {
+  MatDialog,
+  MatDialogRef,
+  MAT_DIALOG_DATA,
+} from '@angular/material/dialog';
+
+export interface DialogData {
+  animal: string;
+  name: string;
+}
+
+@Component({
+  selector: 'dialog-overview-example-dialog',
+  // templateUrl: 'dialog-overview-example-dialog.html',
+  template: `
+    <h1 mat-dialog-title>Hi {{ data.name }}</h1>
+    <div mat-dialog-content>
+      <p>What's your favorite animal?</p>
+      <mat-form-field appearance="fill">
+        <mat-label>Favorite Animal</mat-label>
+        <input matInput [(ngModel)]="data.animal" />
+      </mat-form-field>
+    </div>
+    <div mat-dialog-actions>
+      <button mat-button (click)="onNoClick()">No Thanks</button>
+      <button mat-button [mat-dialog-close]="data.animal" cdkFocusInitial>
+        Ok
+      </button>
+    </div>
+  `,
+})
+export class DialogOverviewExampleDialog {
+  constructor(
+    public dialogRef: MatDialogRef<DialogOverviewExampleDialog>,
+    @Inject(MAT_DIALOG_DATA) public data: DialogData
+  ) {}
+
+  onNoClick(): void {
+    this.dialogRef.close();
+  }
+}
+
+@Component({
+  selector: 'app-remove-fish-farm',
+  templateUrl: './remove-fish-farm.component.html',
+  styleUrls: ['./remove-fish-farm.component.scss'],
+})
+export class RemoveFishFarmComponent {
+  animal: string = '';
+  name: string = '';
+
+  constructor(public dialog: MatDialog) {}
+
+  openDialog(): void {
+    const dialogRef = this.dialog.open(DialogOverviewExampleDialog, {
+      width: '250px',
+      data: { name: this.name, animal: this.animal },
+    });
+
+    dialogRef.afterClosed().subscribe((result) => {
+      console.log('The dialog was closed');
+      this.animal = result;
+    });
+  }
+}
